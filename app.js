4422db023041497246ec9f9a60a91b7f507362b2
satishtrip <satish.tripathi1297@gmail.com>
2021-06-08 00:54:11 +0530

    added login and google auth

---
+++ app.js
@@ -0,0 +1,58 @@
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const session = require('express-session')
+
+const connectDB = require('./config/db')
+
+dotenv.config({path: './config/config.env' })
+connectDB()
+const app = express()
+
+
+//Passport Auth Config
+require('./config/passport')(passport)
+
+
+
+// at the time of loggin , when env set  is to dev
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+  }
+
+//my static folder
+app.use(express.static(path.join(__dirname)))
+
+
+//handlebars
+
+app.engine('.hbs', exphbs({defaultLayout:'main',extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//sessions
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  //cookie: { secure: true }
+}))
+
+//Routes/Urls
+app.use('/',require('./routes/index'))
+app.use('/auth',require('./routes/auth'))
+
+
+  
+const PORT = process.env.PORT || 3000
+
+
+app.listen(PORT, console.log(`Server running on ${process.env.NODE_ENV} node on port ${PORT}`))
---
+++ routes/auth.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const passport = require('passport')
+const router = express.Router()
+
+//@desc Authorisation with google
+//@rout GET /auth/google
+
+router.get('/google', passport.authenticate('google', {scope:['profile']}))
+
+//google auth callback
+//route = get/
+
+router.get(
+    '/google/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),
+    (req, res) => {
+      res.redirect('/dashboard')
+    }
+  )
+  
+//   // @desc    Logout user
+//   // @route   /auth/logout
+//   router.get('/logout', (req, res) => {
+//     req.logout()
+//     res.redirect('/')
+//   })
+  
+
+module.exports = router
